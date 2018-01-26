a288150dec73a641a43bd8e787d4f5c5f6df763f
albanie <albanie@robots.ox.ac.uk>
2018-01-26 13:51:04 +0000

    mods

---
+++ setup_LGBWT.m
@@ -0,0 +1,10 @@
+function setup_LGBWT()
+%SETUP_LGBWT Sets up the code for Learning Grimaces By Watching TV, by adding
+% its folders to the Matlab path
+%
+% Copyright (C) 2016 Samuel Albanie
+% Licensed under The MIT License [see LICENSE.md for details]
+
+  root = fileparts(mfilename('fullpath')) ;
+  addpath(root, [root '/core'], [root '/facevalue-exps'], [root '/figs']) ;
+  addpath([root '/sfew-exps']) ;
