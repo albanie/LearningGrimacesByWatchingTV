23009e0ccc85133eee99cbadd7cddecfc04e3bb9
albanie <albanie@robots.ox.ac.uk>
2018-08-03 19:53:52 +0100

    clean out old code

--- core/IO/saveState.m
+++
@@ -1,7 +0,0 @@
-function saveState(fileName, dagnet, stats)
-% SAVESTATE saves the DagNN object `dagnet` as a vanilla matlab 
-% structure `net`, together with the stats collected so far in 
-% the file specified by `fileName`.
-
-net = dagnet.saveobj();
-save(fileName, 'net', 'stats');
--- core/dag/computeEpoch.m
+++
@@ -1,9 +0,0 @@
-function [dagnet, stats] = computeEpoch(state, dagnet, epoch, stats, opts)
-% COMPUTEEPOCH distributes the processing work for the given epoch
-% according to the number of gpus available.
-
-if opts.numGpus <= 1
-    stats = processSequentially(state, dagnet, epoch, stats, opts);
-else
-    [dagnet, stats] = processInParallel(state, dagnet, epoch, stats, opts);
-end
