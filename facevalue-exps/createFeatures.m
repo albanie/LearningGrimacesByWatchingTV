af136a87aa62e2a285110881fabfc6f5464db5e8
albanie <albanie@robots.ox.ac.uk>
2018-08-04 00:02:03 +0100

    mods

--- facevalue-exps/createFeatures.m
+++ facevalue-exps/createFeatures.m
@@ -26,39 +26,39 @@ if numGpus >= 1
 end
 
 for t = 1:opts.batchSize:50%numel(imdb.images.labels);
-    
+
     % print progress to screen
     fprintf('Progress:  %3d/%3d\n',  ...
         fix((t-1)/opts.batchSize)+1, ...
         ceil(numel(imdb.images.labels)/opts.batchSize)) ;
     batchSize = min(opts.batchSize, numel(imdb.images.labels) - t + 1) ;
-    
+
     % get this image batch and prefetch the next
     batchStart = t;
     batchEnd = min(t+opts.batchSize-1, numel(imdb.images.labels)) ;
     batch = batchStart : batchEnd ;
-    
+
     % once all data has been processed, break out
     if numel(batch) == 0, continue ; end
-    
+
     % load the images and labels
     inputs = getBatch(imdb, batch, 'test', opts, dagnet) ;
-    
+
     % store the values at the feature layer
     dagnet.vars(dagnet.getVarIndex('x24')).precious = true ;
     dagnet.vars(dagnet.getVarIndex('prediction')).precious = true ;
-    
+
     % retrieve the scores
     dagnet.mode = 'test';
     dagnet.eval({inputs{1:2}});
-    
+
     % obtain the CNN otuput
     prediction_score = dagnet.vars(dagnet.getVarIndex('prediction')).value ;
     prediction_score = squeeze(gather(prediction_score)) ;
-    
+
     fc_score = dagnet.vars(dagnet.getVarIndex('x24')).value ;
     fc_score = squeeze(gather(fc_score)) ;
-    
+
     % show the classification results
 %     [bestScore, best] = max(scores) ;
     prediction_scores(batch,:) = prediction_score';
