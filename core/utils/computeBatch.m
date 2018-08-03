3dc684ad58a53917792b631c52923ad5a64a4a89
albanie <albanie@robots.ox.ac.uk>
2018-08-03 19:56:37 +0100

    cleanup

--- core/utils/computeBatch.m
+++
@@ -1,7 +0,0 @@
-function batch = computeBatch(subsetIdx, subBatchIdx, subset, opts)
-% COMPUTEBATCH computes the indices to be used in the next batch of 
-% data.
-batchStart = subsetIdx + (labindex-1) + (subBatchIdx-1) * numlabs ;
-batchEnd = min(subsetIdx + opts.batchSize-1, numel(subset));
-batch = subset(batchStart : opts.numSubBatches * numlabs : batchEnd);
-end
